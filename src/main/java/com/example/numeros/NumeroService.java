6265fbb34a611e221957ab76c16a6ac922af9397
mateo <mateozacar@gmail.com>
2023-03-09 20:46:40 -0500

    Initial commit

---
+++ src/main/java/com/example/numeros/NumeroService.java
@@ -0,0 +1,28 @@
+package com.example.numeros;
+
+public class NumeroService {
+
+    public Integer obtenerTotal(Integer n) {
+        /** El numero 13 de la mala suerte
+         * Crear un algoritmo que pasado un numero n debe retonar
+         * la cantidad de numeros que hay sin contar los que contengan
+         * el 13,
+         *
+         * Ejemplo 1
+         * numero = 100
+         * numeros a descartar =  13
+         * total numeros: 99
+         *
+         * Ejemplo 2:
+         * numero = 1000
+         * numeros a descartar =  13, 130...139
+         * total numeros: 989
+         *
+         * Ejemplo 2:
+         * numero = 3800
+         * numeros a descartar = 13, 130..139 , 1300...1399.
+         * total numeros: 3699
+         */
+        return n;
+    }
+}
